0d50462f259ccf76fd2201ff1c2764578b701991
Ed <eduard@kamenskikh.me>
2024-07-05 02:52:39 +0400

    Initial commit

---
+++ src/main/java/pro/sky/spring_mockito/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
